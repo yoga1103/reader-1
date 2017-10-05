b6be9c5678d97ac4e94f84670f4032aa4f8a0266
batman <afshaanlondhe4@gmail.com>
2017-10-05 14:21:21 +0530

    uses REST api now

--- app/src/main/java/com/example/shaany/reader/Post.java
+++ app/src/main/java/com/example/shaany/reader/Post.java
@@ -1,5 +1,6 @@
 package com.example.shaany.reader;
 
+
 import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,7 +15,16 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
+
 import java.util.Map;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import static android.R.attr.id;
+
 
 public class Post extends AppCompatActivity {
     TextView title;
@@ -22,8 +32,8 @@ public class Post extends AppCompatActivity {
     ProgressDialog progressDialog;
     Gson gson;
     Map<String, Object> mapPost;
-    Map<String, Object> mapTitle;
     Map<String, Object> mapContent;
+    String selection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,27 +41,41 @@ public class Post extends AppCompatActivity {
         setContentView(R.layout.post);
 
         final String id = getIntent().getExtras().getString("id");
+        final String div = getIntent().getExtras().getString("div");
 
         title = (TextView) findViewById(R.id.title);
-        content = (WebView)findViewById(R.id.content);
+        content = (WebView) findViewById(R.id.content);
+        content.getSettings().setJavaScriptEnabled(true);
 
         progressDialog = new ProgressDialog(Post.this);
         progressDialog.setMessage("Loading...");
         progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
         progressDialog.show();
 
-        String url = "http://www.thejavaprogrammer.com/wp-json/wp/v2/posts/"+id+"?fields=title,content";
+        String url = "https://bahainf.mystagingwebsite.com/wp-json/wp/v2/pages/" + id + "";
 
         StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String s) {
                 gson = new Gson();
                 mapPost = (Map<String, Object>) gson.fromJson(s, Map.class);
-                mapTitle = (Map<String, Object>) mapPost.get("title");
                 mapContent = (Map<String, Object>) mapPost.get("content");
-
-                title.setText(mapTitle.get("rendered").toString());
-                content.loadData(mapContent.get("rendered").toString(),"text/html","UTF-8");
+                //get the specific html to display
+                selection = mapContent.toString();
+                Document doc = Jsoup.parse(selection, "UTF-8");
+                //if the div name is cat
+                if (div.equals("cat")) {
+                    Elements stuff = doc.getAllElements();
+                    //display it
+                    String load = stuff.toString();
+                    content.loadData(load, "text/html", "UTF-8");
+                    //if the div name isn't cat
+                } else {
+                    Element stuff = doc.getElementById(div);
+                    //display it
+                    String load = stuff.toString();
+                    content.loadData(load, "text/html", "UTF-8");
+                }
 
                 progressDialog.dismiss();
             }
